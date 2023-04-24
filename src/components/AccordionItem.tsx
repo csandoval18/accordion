d3f60088486fb209bfcbf69d1dae925706950a50
Christian Sandoval <christiansandoval1018@outlook.com>
2023-04-23 19:34:30 -0500

    updated accordion

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -1,37 +1,67 @@
-import React, { useState, useRef } from "react"
-import { AiOutlineClose, AiOutlinePlus } from "react-icons/ai"
+import React, { useState, useRef } from 'react';
+import {
+	AiOutlineClose,
+	AiOutlinePlus,
+} from 'react-icons/ai';
 
 interface AccordionItemProps {
-	title?: string
-	bodyContent?: string
+	title?: string;
+	bodyContent?: string;
 }
 
 const AccordionItem: React.FC<AccordionItemProps> = (
 	accordionProps: AccordionItemProps,
 ) => {
-	const [active, setActive] = useState<true | false>(false)
-	const contentEl = useRef<HTMLDivElement>(null)
+	const [active, setActive] = useState<true | false>(
+		false,
+	);
+	const contentEl = useRef<HTMLDivElement>(null);
 	return (
 		<div className='accordion-item'>
 			<div
-				className={`btn-accordion ${active ? "active" : ""}`}
+				className={`btn-accordion ${
+					active ? 'active' : ''
+				}`}
 				onClick={() => {
-					setActive(!active)
+					setActive(!active);
 				}}
 			>
-				{accordionProps.title}
+				<span className='accordion-title'>
+					{accordionProps.title}
+				</span>
 				<span className='icon'>
-					{active ? <AiOutlineClose /> : <AiOutlinePlus />}
+					{active ? (
+						<svg
+							stroke='currentColor'
+							fill='currentColor'
+							stroke-width='0'
+							viewBox='0 0 1024 1024'
+							version='1.1'
+							height='1em'
+							width='1em'
+							xmlns='http://www.w3.org/2000/svg'
+						>
+							<defs></defs>
+							<path d='M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z'></path>
+							<path d='M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z'></path>
+						</svg>
+					) : (
+                        'test'
+					)}
 				</span>
 				<div
-					className={`accordion-panel ${active ? "active" : ""}`}
+					className={`accordion-panel ${
+						active ? 'active' : ''
+					}`}
 					ref={contentEl}
 				>
-					<p>{accordionProps.bodyContent}</p>
+					<p className=''>
+						{accordionProps.bodyContent}
+					</p>
 				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default AccordionItem
+export default AccordionItem;
